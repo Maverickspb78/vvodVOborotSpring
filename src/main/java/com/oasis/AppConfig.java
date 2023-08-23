de46ad237c99e38f9458656fd5ff6b49c7236b36
Maverickspb78 <Maverickspb78@gmail.com>
2023-08-23 10:02:11 +0300

    Initial commit

---
+++ src/main/java/com/oasis/AppConfig.java
@@ -0,0 +1,9 @@
+package com.oasis;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.oasis")
+public class AppConfig {
+}
---
+++ src/main/java/com/oasis/Main.java
@@ -0,0 +1,7 @@
+package com.oasis;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
